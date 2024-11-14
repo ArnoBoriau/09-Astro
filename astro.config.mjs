1ad8505679f19fdd37a28402dc88205bfd96e82c
Arno Boriau <arno.boriau@student.howest.be>
2024-11-14 21:52:09 +0100

    Astro Island with Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://09-astro.netlify.app"
+  site: "https://09-astro.netlify.app",
+  integrations: [preact()]
 });
-
